b8ed3f0163f63b81e0a838c5df078ff2f1a8002b
sanjai3102001 <sanjai99522s@gmail.com>
2026-02-11 23:52:59 +0530

    double the linked list o(1)

---
+++ LinkedList/DoubleTheLinkedList/DoubleTheLinkedList.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	// I am constructing a linkedlist
+	arr := []int{1, 8, 9}
+	head := &ListNode{Val: arr[0]}
+	backuphead := head
+	for i := 1; i < len(arr); i++ {
+		head.Next = &ListNode{Val: arr[i]}
+		head = head.Next
+	}
+	head = backuphead
+
+	// Here I am appending it to a string
+	if head.Val > 4 {
+		newhead := &ListNode{Val: 0, Next: head}
+		head = newhead
+	}
+
+	current := head
+
+	for current != nil {
+
+		current.Val = (current.Val * 2) % 10
+		if current.Next != nil && current.Next.Val > 4 {
+			current.Val += 1
+		}
+		current = current.Next
+	}
+
+	// Now just prinitng the list
+	for head != nil {
+		fmt.Println(head.Val)
+		head = head.Next
+	}
+}
